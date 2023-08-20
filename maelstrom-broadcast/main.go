55fa3c0992d8a8e901c3630470f17a9e046d3f5c
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-08-20 15:05:55 +0100

    setup broadcast

---
+++ maelstrom-broadcast/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Store the message in body["message"]
+		//
+		body["type"] = "broadcast_ok"
+
+		return n.Reply(msg, body)
+		// n.RPC
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "read_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "topology_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	// Execute the node's message loop. This will run until STDIN is closed.
+	if err := n.Run(); err != nil {
+		log.Printf("ERROR: %s", err)
+		os.Exit(1)
+	}
+}
