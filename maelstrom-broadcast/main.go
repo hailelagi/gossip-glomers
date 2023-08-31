d589b45efafb2d9178b4957419e11c212e3d02bd
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-08-31 10:54:10 +0100

    wip multi-node broadcast

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 
+	var store []int
+
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 
 		var body map[string]any
@@ -19,11 +21,14 @@ func main() {
 		}
 
 		// Store the message in body["message"]
-		//
+		store = append(store, body["message"].(int))
 		body["type"] = "broadcast_ok"
 
-		return n.Reply(msg, body)
-		// n.RPC
+		for _, node := range n.NodeIDs() {
+			n.Send(node, body)
+		}
+
+		return nil
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
@@ -34,8 +39,14 @@ func main() {
 		}
 
 		body["type"] = "read_ok"
+		body["messages"] = store
 
-		return n.Reply(msg, body)
+		for _, node := range n.NodeIDs() {
+			return n.RPC(node, body, func(msg maelstrom.Message) error {
+				return nil
+			})
+		}
+		return nil
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
@@ -47,7 +58,11 @@ func main() {
 
 		body["type"] = "topology_ok"
 
-		return n.Reply(msg, body)
+		for _, node := range n.NodeIDs() {
+			n.Send(node, body)
+		}
+
+		return nil
 	})
 
 	// Execute the node's message loop. This will run until STDIN is closed.
---
+++ maelstrom-broadcast/test.sh
@@ -0,0 +1,1 @@
+go install . && ../maelstrom/maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 1 --time-limit 1 --rate 1
