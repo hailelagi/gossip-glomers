c14470c7af41fb09a586ef82a8a34036a9376124
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2023-09-06 23:01:06 +0100

    more wip, wtf parsing

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -8,10 +8,19 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type broadcastMessage struct {
+	Message int `json:message`
+}
+
+type topologyMessage struct {
+	Topology map[string][]string `json:"topology"`
+}
+
 func main() {
 	n := maelstrom.NewNode()
 
-	var store []int
+	// todo: wrap store with mutex across concurrenct sessions
+	var store []float64
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 
@@ -21,14 +30,17 @@ func main() {
 		}
 
 		// Store the message in body["message"]
-		store = append(store, body["message"].(int))
+		store = append(store, body["message"].(float64))
 		body["type"] = "broadcast_ok"
 
+		// TODO: make async/parallelise with go routines
 		for _, node := range n.NodeIDs() {
-			n.Send(node, body)
+			if node != n.ID() {
+				n.Send(node, body)
+			}
 		}
 
-		return nil
+		return n.Reply(msg, body)
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
@@ -41,28 +53,19 @@ func main() {
 		body["type"] = "read_ok"
 		body["messages"] = store
 
-		for _, node := range n.NodeIDs() {
-			return n.RPC(node, body, func(msg maelstrom.Message) error {
-				return nil
-			})
-		}
-		return nil
+		return n.Reply(msg, body)
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
 
-		var body map[string]any
+		var body topologyMessage
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		body["type"] = "topology_ok"
-
-		for _, node := range n.NodeIDs() {
-			n.Send(node, body)
-		}
-
-		return nil
+		res := make(map[string]interface{})
+		res["type"] = "topology_ok"
+		return n.Reply(msg, res)
 	})
 
 	// Execute the node's message loop. This will run until STDIN is closed.
