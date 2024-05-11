d1e27325f74a05b4d3b8a70331d702fc5c96eec7
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-05-11 04:05:26 +0100

    wip g counter
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- maelstrom-g-counter/main.go
+++ maelstrom-g-counter/main.go
@@ -4,25 +4,33 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+	"sync/atomic"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+var gCounter int64
+
 func main() {
 	n := maelstrom.NewNode()
 
-	// Register a handler for the "echo" message that responds with an "echo_ok".
-	n.Handle("echo", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as an loosely-typed map.
+	n.Handle("add", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		// Update the message type.
-		body["type"] = "echo_ok"
+		delta := int64(body["delta"].(float64))
+		atomic.AddInt64(&gCounter, delta)
 
-		// Echo the original message back with the updated message type.
+		return n.Reply(msg, map[string]any{"type": "add_ok"})
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body = map[string]any{"type": "read_ok"}
+
+		delta := atomic.LoadInt64(&gCounter)
+		body["value"] = delta
 
 		return n.Reply(msg, body)
 	})
