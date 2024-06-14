6fd0983e64d18c7739ca1328fec06f7838d19f0b
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-06-15 00:49:57 +0100

    wip commutative g counter
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- maelstrom-g-counter/main.go
+++ maelstrom-g-counter/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"os"
-	"sync/atomic"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-var gCounter int64
-
 func main() {
 	n := maelstrom.NewNode()
+	kv := maelstrom.NewSeqKV(n)
 
 	n.Handle("add", func(msg maelstrom.Message) error {
 		var body map[string]any
@@ -20,8 +19,27 @@ func main() {
 			return err
 		}
 
+		ctx := context.Background()
 		delta := int64(body["delta"].(float64))
-		atomic.AddInt64(&gCounter, delta)
+
+		// the operation is addition and is commutative
+		// as it is a counter that only ever grows
+		prev, e := kv.ReadInt(ctx, "delta")
+
+		if e != nil {
+			prev = 0
+		}
+
+		result := int64(prev) + delta
+		err := kv.CompareAndSwap(ctx, "delta", delta, result, true)
+
+		for _, dest := range n.NodeIDs() {
+			go n.SyncRPC(ctx, dest, map[string]any{"type": "add", "delta": delta})
+		}
+
+		if err == nil {
+			log.Fatalf("could not update increment only counter")
+		}
 
 		return n.Reply(msg, map[string]any{"type": "add_ok"})
 	})
@@ -29,9 +47,14 @@ func main() {
 	n.Handle("read", func(msg maelstrom.Message) error {
 		var body = map[string]any{"type": "read_ok"}
 
-		delta := atomic.LoadInt64(&gCounter)
-		body["value"] = delta
+		ctx := context.Background()
+		delta, err := kv.Read(ctx, "delta")
 
+		if err == nil {
+			log.Fatalf("could not read counter")
+		}
+
+		body["value"] = delta.(float64)
 		return n.Reply(msg, body)
 	})
 
