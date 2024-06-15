994928a7813c74c937952f5944d3b82856b84335
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-06-15 03:24:17 +0100

    stub all txn + kafka
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

---
+++ maelstrom-txn/read-uncommited-safe.sh
@@ -0,0 +1,1 @@
+go install . && ../maelstrom/maelstrom test -w txn-rw-register --bin ~/go/bin/maelstrom-txn --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --consistency-models read-uncommitted --availability total --nemesis partition
