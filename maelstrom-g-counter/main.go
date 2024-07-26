93db0e6150fe5ac39a4ddb4ab80dc14b28dbe5df
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-07-26 14:42:11 +0100

    stub: pseudo crdt
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- maelstrom-g-counter/main.go
+++ maelstrom-g-counter/main.go
@@ -26,6 +26,21 @@ type retry struct {
 	err     error
 }
 
+/*
+1: payload Payload type; instantiated at all replicas
+2: initial Initial value
+3: query Source-local operation (arguments) : returns
+4: pre Precondition
+5: let Execute at source, synchronously, no side effects
+6: update Global update (arguments) : returns
+7: atSource (arguments) : returns
+8: pre Precondition at source
+9: let 1st phase: synchronous, at source, no side effects
+10: downstream (arguments passed downstream)
+11: pre Precondition against downstream state
+12: 2nd phase, asynchronous, side-effects to downstream state
+*/
+
 // the operation is addition and is commutative
 // as it is a counter that only ever grows
 func (s *session) addOperationHandler(msg maelstrom.Message) error {
--- maelstrom-g-counter/state.go
+++ maelstrom-g-counter/state.go
@@ -12,6 +12,22 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+/*
+1: payload Payload type; instantiated at all replicas
+2: initial Initial value
+3: query Query (arguments) : returns
+4: pre Precondition
+5: let Evaluate synchronously, no side effects
+6: update Source-local operation (arguments) : returns
+7: pre Precondition
+8: let Evaluate at source, synchronously
+9: Side-effects at source to execute synchronously
+10: compare (value1, value2) : boolean b
+11: Is value1 ≤ value2 in semilattice?
+12: merge (value1, value2) : payload mergedValue
+13: LUB merge of value1 and value2, at any replica
+*/
+
 func (s *session) addStateTransformHandler(msg maelstrom.Message) error {
 	var wg sync.WaitGroup
 	var result int
