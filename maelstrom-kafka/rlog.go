d61bd557e48135c5e634b30eb7a065dd8fe11030
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-08-04 19:27:32 +0100

    initial global lock
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- maelstrom-kafka/rlog.go
+++ maelstrom-kafka/rlog.go
@@ -34,6 +34,9 @@ func NewLog(partitions int) *replicatedLog {
 
 // Append a k/v entry to the log and returns the last index offset
 func (l *replicatedLog) Append(key, value any) int {
+	l.global.Lock()
+	defer l.global.Unlock()
+
 	offset := len(l.log) + 1
 
 	event := entry{key: key.(string), value: value.(float64), offset: offset}
@@ -45,6 +48,9 @@ func (l *replicatedLog) Append(key, value any) int {
 
 // Read messages from a set of logs starting from the given offset in each log
 func (l *replicatedLog) Read(offsets map[string]any) map[string][][]float64 {
+	l.global.RLock()
+	defer l.global.RUnlock()
+
 	var result = make(map[string][][]float64)
 
 	for key, offset := range offsets {
@@ -54,28 +60,26 @@ func (l *replicatedLog) Read(offsets map[string]any) map[string][][]float64 {
 	return result
 }
 
-/*
-func (l *replicatedLog) Read(key any) any {
-	start, end := 0, len(l.log)-1
-
-	for start <= end {
-		mid := start + (end-start)/2
-
-		if l.log[mid] == key {
-			return mid
-		} else if l.log[mid] < key {
-			start = mid + 1
-		} else {
-			end = mid - 1
-		}
-	}
-
-	return -1
-}
-*/
-
 func (l *replicatedLog) seek(key string, beginIdx int) [][]float64 {
 	var result [][]float64
+	/*
+		start, end := 0, len(l.log)-1
+
+
+			for start <= end {
+				mid := start + (end-start)/2
+
+				if l.log[mid] == key {
+					return mid
+				} else if l.log[mid] < key {
+					start = mid + 1
+				} else {
+					end = mid - 1
+				}
+			}
+
+			return -1
+	*/
 
 	for _, offset := range l.version[key] {
 		if offset >= beginIdx {
@@ -90,6 +94,9 @@ func (l *replicatedLog) seek(key string, beginIdx int) [][]float64 {
 
 // HasCommitted Checks if the client and log are in sync
 func (l *replicatedLog) HasCommitted(offsets map[string]any) bool {
+	l.global.RLock()
+	defer l.global.RUnlock()
+
 	var IsCommitted bool
 
 	for key, offset := range offsets {
@@ -108,6 +115,9 @@ func (l *replicatedLog) HasCommitted(offsets map[string]any) bool {
 
 // ListCommitted returns the last offset for a client
 func (l *replicatedLog) ListCommitted(keys []any) map[string]any {
+	l.global.RLock()
+	defer l.global.RUnlock()
+
 	var result = make(map[string]any)
 
 	for _, key := range keys {
